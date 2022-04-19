a401eed6a1b2b5b71776f36e2cb69158e38292fd
ChuaHZ <103973487+ChuaHZ@users.noreply.github.com>
2022-04-19 16:26:17 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -11,6 +11,9 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
-    
+    int a = in.nextInt();
+    a = a * a;
+    System.out.println(a);
+    in.close();
   }
 }
